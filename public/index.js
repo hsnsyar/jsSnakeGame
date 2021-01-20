81f09274c5a77e0c3608f8cf61a84f0d29f5ffe1
Hüsnü Sayar <53179795+husnu-sayar@users.noreply.github.com>
2021-01-20 12:45:11 +0300

    first

---
+++ public/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+//const HTML_DIR = path.join(__dirname, '/public/')
+//app.use(express.static(HTML_DIR))
+//app.use(express.static(__dirname + '/public/'));
+app.use("/js",express.static('./js/'));  
+
+//app.use(express.static(__dirname + '/public/'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
